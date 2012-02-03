5809dd8cf1d9e7c124cdd7aff982947cfa6adbde
Kyle Foreman <kyleforeman@gmail.com>
2012-02-03 00:02:24 +0000

    forgot to change the full path in batch file

--- GBDx_app.bat
+++ GBDx_app.bat
@@ -1,8 +1,8 @@
 cd %~dp0
 
 REM use python as host (Chrome restricts file system access)
-START /b ..\offline\python\app\python.exe -m SimpleHTTPServer 8888
+START /b %~dp0\offline\python\app\python.exe -m SimpleHTTPServer 8888
 
 REM start Chrome in application mode (no toolbars)
 REM you may have to change the location to Chrome below 
-..\offline\PortableGoogleChrome\Chrome\chrome.exe --enable-file-access-from-files --allow-running-insecure-content --app="http://localhost:8888"
+%~dp0\offline\chrome\Chrome\chrome.exe --enable-file-access-from-files --allow-running-insecure-content --app="http://localhost:8888"
